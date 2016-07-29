a3bbe88390efbbd207976254f55ad9e66ca38ae4
vanstorm9 <antlowhur@yahoo.com>
2016-07-29 11:01:40 -0500

    Constructed a button string

--- sample-xml-parser.py
+++ sample-xml-parser.py
@@ -3,23 +3,44 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('sparc_sample.xml')
 root = tree.getroot()
 
+def buttonCheck(xmlElement, direction):
+        # Going to generate HTML buttons
+        # <button type="submit" class="btn btn-default navbar-btn" id="btn_getDrawing" value="getDrawing">  Draw</button>
+        print 'On the ', direction, ' side. . .'
+        if xmlElement.tag == 'button':
+                
+                butID = ''
+                butTxt = ''
+                butClass = 'class="btn btn-default navbar-btn"'
+                
+                for butProp in xmlElement:
+                        if butProp.tag == 'id':
+                                butID = butProp.text
+                        elif butProp.tag == 'text':
+                                butTxt = butProp.text
+                        elif butProp.tag == 'behavior':
+                                for behaveChildren in butProp:
+                                        z = 1 + 1
+                                        #print behaveChildren.tag
+                
+                buttonHTML = '<button '+ butClass +'id="'+ butID +'">'+ butTxt +'</button>'
+                print buttonHTML
+
 for child in root:
         #print child.tag, child.attrib
-        print child.tag
+        #print child.tag
 
         # We are going to try to e
         if child.tag == 'navbar':
             for navbarChild in child:
-                print navbarChild.tag
+                #print navbarChild.tag
 
                 if navbarChild.tag == 'left':
                     for leftElement in navbarChild:
-                        print leftElement.tag
 
+                        buttonCheck(leftElement, navbarChild.tag)
                     
                 elif navbarChild.tag == 'right':
                     for rightElement in navbarChild:
-                        print rightElement.tag
-                    
-        
+                        buttonCheck(rightElement, navbarChild.tag)
  
